1b7a14cf0eaa18be8d1be54b8b6d9b9299bd1a9d
Pedro Barbeira <pedro.barbeira95@gmail.com>
2022-05-25 02:19:49 +0100

    qua 25 mai 2022 02:19:49 WEST

--- project/cypher.c
+++ project/cypher.c
@@ -66,21 +66,6 @@ cypher* loadCypher(cypher* cyphr){
 }
 
 /**
- * @brief Sees if a string has punctuation in it
- * 
- * @param where the index where the punctuation starts
- * @return int 0 if there is punctuation, 1 otherwise
- */
-int hasPunct(int *where){
-    for(int i = 0; i < strlen(wordBuffer); i++)
-        if(ispunct(wordBuffer[i])){
-            *where = i;
-            return 0;
-        }
-    return 1;
-}
-
-/**
  * @brief Compares two words to see if they're the same
  * 
  * @param w1 the first word
@@ -89,19 +74,19 @@ int hasPunct(int *where){
  */
 int cmpwrd(char* w1, char* w2){
     int where;
-    if(strlen(w1) == strlen(w2)) return (!strcmp(w1, w2));
+    if(strlen(w1) == strlen(w2) || strlen(w1) < strlen(w2)) return (!strcmp(w1, w2));
     else{
         int i = 0, size;
         if(strlen(w1) < strlen(w2)) size = strlen(w1);
         else size = strlen(w2);
         while(i < size){
-            if(w2[i] == w1[0]) break;
+            if(w1[i] == w2[0]) break;
             else i++;
         }
-        if(i == size-1) return 0;
+        if(i == size) return 0;
         else{
             for(int j = 0; j < size; j++)
-                if(w2[i+j] != w1[j]) return 0;
+                if(w1[i+j] != w2[j]) return 0;
         }
         return 1; 
     }
@@ -134,8 +119,13 @@ int compareCypher(cypher* cyphr, int* index, int* target){
     return 1;
 }
 
+/**
+ * @brief Cyphers the contents of wordBuffer
+ * 
+ * @param cypher the word to be used as cypher
+ */
 void cypherWord(char* cypher){
-    char* word;
+    char* word = (char*)malloc(sizeof(char)*WORDSIZE);
     strcpy(word, wordBuffer);
     flush(wordBuffer, WORDSIZE);
     int j, i=0;
@@ -148,76 +138,12 @@ void cypherWord(char* cypher){
         i++;
     }
     for(;j < strlen(word);j++){
-        if(ispunct(word[j])){
+        if(ispunct(word[j]) || isspace(word[j])){
             wordBuffer[i] = word[j];
             i++;
         }
     }
-}
-
-/**
- * @brief Cyphers words when there's punctuation at the start of the string
- * 
- * @param cyphr the cypher table
- * @param index index of the cypher table to be accessed
- * @param target if 0 swaps target for swap, if 1 swaps swap for target
- */
-void handlePrefix(cypher* cyphr, int index, int target){
-    char lastChar = wordBuffer[strlen(wordBuffer)-1];
-    int i=1;
-    char* shift;
-    if(target) shift = cyphr->table[index].target;
-    else shift = cyphr->table[index].swap;
-    for(;i<strlen(shift);i++)
-        wordBuffer[i]=shift[i];
-    wordBuffer[i] = lastChar;
-    wordBuffer[i+1]='\0';
-}
-
-/**
- * @brief Cyphers words when there's punctuation at the end of the string
- * 
- * @param cyphr the cypher table
- * @param index the index of the cypher table to be accessed
- * @param where index of the string where punctuation starts
- * @param target if 0 swaps target for swap, if 1 swaps swap for target
- */
-void handleSuffix(cypher* cyphr, int index, int where, int target){
-    char punct[PBUFFSIZE];
-    int i = where;
-    for(;wordBuffer[i]!='\0';i++){
-        punct[i-where]=wordBuffer[i];
-    }
-    punct[i-where] = '\0';
-    char*shift;
-    if(target) shift = cyphr->table[index].target;
-    else shift = cyphr->table[index].swap;
-    int usedBuffer = strlen(wordBuffer);
-    for(i=0;i<strlen(shift);i++)
-        wordBuffer[i]=shift[i];
-    for(int j=0;punct[j]!='\0';j++){
-        wordBuffer[i+j]=punct[j];
-    }
-    wordBuffer[i+strlen(punct)]='\0';
-}
-
-/**
- * @brief Cyphers words when there is no punctuation involved
- * 
- * @param cyphr the cypher table
- * @param index the index of the cypher table
- * @param target if 0 swaps target for swap, if 1 swaps swap for target
- */
-void handleNoffix(cypher* cyphr, int index, int target){
-    char lastChar = wordBuffer[strlen(wordBuffer)-1];
-    int i=0;
-    char*shift;
-    if(target) shift = cyphr->table[index].target;
-    else shift = cyphr->table[index].swap;
-    for(;i<strlen(shift);i++)
-        wordBuffer[i]=shift[i];
-    wordBuffer[i]=lastChar;
-    wordBuffer[i+1]='\0';
+    free(word);
 }
 
 /**
@@ -241,17 +167,10 @@ char* cypherText(cypher* cyphr, char* text){
             foundWord=1;
         }
         if(foundWord){
-            int i, target;
-            if(!compareCypher(cyphr, &i, &target)){
-                if(!target) cypherWord(cyphr->table[i].target);
-                else cypherWord(cyphr->table[i].swap);
-                /*
-                int where;
-                if(!hasPunct(&where)){
-                    if(!where) handlePrefix(cyphr, index, target);
-                    else handleSuffix(cyphr, index, where, target);
-                }
-                else handleNoffix(cyphr, index, target);*/
+            int index, target;
+            if(!compareCypher(cyphr, &index, &target)){
+                if(!target) cypherWord(cyphr->table[index].swap);
+                else cypherWord(cyphr->table[index].target);
             }
             strcat(cypheredText, wordBuffer);
             foundWord=0;
--- project/test.c
+++ project/test.c
@@ -3,15 +3,12 @@
 #include <ctype.h>
 #include <string.h>
 
-#define WORDSIZE 64
-
-char wordBuffer[64];
-
-void flush(char* buffer, int buffersize){
-    for(int i = 0; i <buffersize;i++)
-        buffer[i]='\0';
-}
-
+/**
+ * @brief Sees if a string has punctuation in it
+ * 
+ * @param where the index where the punctuation starts
+ * @return int 0 if there is punctuation, 1 otherwise
+ */
 int hasPunct(int *where){
     for(int i = 0; i < strlen(wordBuffer); i++)
         if(ispunct(wordBuffer[i])){
@@ -21,31 +18,97 @@ int hasPunct(int *where){
     return 1;
 }
 
-
-void cypherWord(char* cypher){
-    char* word;
-    strcpy(word, wordBuffer);
-    flush(wordBuffer, WORDSIZE);
-    int where;
-    if(!hasPunct(&where)) copyWord(cypher, 0);
-    else{
-        int j, i = 0;
-        if(!where){
-            for(j=0;ispunct(word[j]);j++){
-             wordBuffer[i] = word[j];
-             i++;
-            }
-        }
-        copyWord(cypher, where);
-        for(j = where;j < strlen(word);j++){
-            if(ispunct(word[j])){
-                wordBuffer[i+] = word[j];
-                i++;
-            }
-        }
-    }
+/**
+ * @brief Cyphers words when there's punctuation at the start of the string
+ * 
+ * @param cyphr the cypher table
+ * @param index index of the cypher table to be accessed
+ * @param target if 0 swaps target for swap, if 1 swaps swap for target
+ */
+void handlePrefix(cypher* cyphr, int index, int target){
+    char lastChar = wordBuffer[strlen(wordBuffer)-1];
+    int i=1;
+    char* shift;
+    if(target) shift = cyphr->table[index].target;
+    else shift = cyphr->table[index].swap;
+    for(;i<strlen(shift);i++)
+        wordBuffer[i]=shift[i];
+    wordBuffer[i] = lastChar;
+    wordBuffer[i+1]='\0';
 }
 
+/**
+ * @brief Cyphers words when there's punctuation at the end of the string
+ * 
+ * @param cyphr the cypher table
+ * @param index the index of the cypher table to be accessed
+ * @param where index of the string where punctuation starts
+ * @param target if 0 swaps target for swap, if 1 swaps swap for target
+ */
+void handleSuffix(cypher* cyphr, int index, int where, int target){
+    char punct[PBUFFSIZE];
+    int i = where;
+    for(;wordBuffer[i]!='\0';i++){
+        punct[i-where]=wordBuffer[i];
+    }
+    punct[i-where] = '\0';
+    char*shift;
+    if(target) shift = cyphr->table[index].target;
+    else shift = cyphr->table[index].swap;
+    int usedBuffer = strlen(wordBuffer);
+    for(i=0;i<strlen(shift);i++)
+        wordBuffer[i]=shift[i];
+    for(int j=0;punct[j]!='\0';j++){
+        wordBuffer[i+j]=punct[j];
+    }
+    wordBuffer[i+strlen(punct)]='\0';
+}
+
+/**
+ * @brief Cyphers words when there is no punctuation involved
+ * 
+ * @param cyphr the cypher table
+ * @param index the index of the cypher table
+ * @param target if 0 swaps target for swap, if 1 swaps swap for target
+ */
+void handleNoffix(cypher* cyphr, int index, int target){
+    char lastChar = wordBuffer[strlen(wordBuffer)-1];
+    int i=0;
+    char*shift;
+    if(target) shift = cyphr->table[index].target;
+    else shift = cyphr->table[index].swap;
+    for(;i<strlen(shift);i++)
+        wordBuffer[i]=shift[i];
+    wordBuffer[i]=lastChar;
+    wordBuffer[i+1]='\0';
+}
+
+int cmpwrd(char* w1, char* w2){
+    int where;
+    if(strlen(w1) == strlen(w2) || strlen(w1) < strlen(w2)) return (!strcmp(w1, w2));
+    
+    else{
+        int i = 0, size;
+        if(strlen(w1) < strlen(w2)) size = strlen(w1);
+        else size = strlen(w2);
+        while(i < size){
+            printf("%c %c\n", w1[i], w2[0]);
+            if(w1[i] == w2[0]) break;
+            else i++;
+        }
+        printf("%d\n", i);
+        if(i == size) return 0;
+        else{
+            for(int j = 0; j < size; j++){
+                printf("%c %c\n", w1[i+j], w2[j]);
+                if(w1[i+j] != w2[j]) {
+                    return 0;
+                }
+            }
+        }
+        return 1; 
+    }
+}
 
 int main(){
     char* test1 = "test";
@@ -54,10 +117,7 @@ int main(){
     char* test4 = ".test.";
     char* cypher = "passed";
 
-    copyWord(test1, 0);
-    cypherWord(cypher);
-
-    printf("%s\n", wordBuffer);
+    printf("%lu\n",strlen("1234 "));
 
     return 0;
 }
